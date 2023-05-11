8032358af135ba57867bca4abc20c029b8f1b5cb
Marcus Vinicius <5ucr4m@gmail.com>
2023-05-11 11:44:02 -0300

    chore: adding module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@store': './src/store',
+            '@utils': './src/utils',
+            '@src': './src',
+          }
+        }
+      ]
+    ]
   };
 };
